15610438a7353af4d7d73bd1af2a3cebd80d93c6
g.maxia@gmail.com <g.maxia@gmail.com@d8f13b22-7da2-2874-8a57-a883d836a351>
2012-10-31 08:09:17 +0000

    Added simple_services to the package, to simplify deployment

--- cookbook/show_cluster.sh
+++ cookbook/show_cluster.sh
@@ -19,12 +19,12 @@ fi
 
 for NODE in ${MASTERS[*]}
 do
-    SERVICE=$($TREPCTL -host $NODE services |simple_services -r master -a list)
+    SERVICE=$($TREPCTL -host $NODE services |$SIMPLE_SERVICES -r master -a list)
     $TREPCTL -host $NODE -service $SERVICE heartbeat
 done
 
 for NODE in ${ALL_NODES[*]}
 do
     echo "# node $NODE"
-    $TREPCTL -host $NODE services | simple_services
+    $TREPCTL -host $NODE services | $SIMPLE_SERVICES
 done
