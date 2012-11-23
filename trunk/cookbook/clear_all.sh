50f112f6d3803d60f8eca07a772381329070b334
neil.armitage@continuent.com <neil.armitage@continuent.com@d8f13b22-7da2-2874-8a57-a883d836a351>
2012-11-23 10:11:16 +0000

    Script to reset hosts from an unknown state

---
+++ trunk/cookbook/clear_all.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# (C) Copyright 2012 Continuent, Inc - Released under the New BSD License
+# Version 1.0.3 - 2012-11-19
+
+
+if [ ! -f ./cookbook/USER_VALUES.sh ]
+then
+    echo "./cookbook/USER_VALUES.sh not found"
+    exit 1
+fi
+
+. ./cookbook/USER_VALUES.sh NODES_MASTER_SLAVE.sh
+
+MYSQL="mysql -u $DATABASE_USER -p$DATABASE_PASSWORD -P $DATABASE_PORT"
+for NODE in ${ALL_NODES[*]} 
+do 
+    ssh $NODE "if [ ! -d $TUNGSTEN_BASE ] ; then mkdir -p $TUNGSTEN_BASE ;  fi" 
+    ssh $NODE "if [ -x $REPLICATOR ] ; then $REPLICATOR stop;  fi" 
+    ssh $NODE rm -rf $TUNGSTEN_BASE/*  
+    for D in $($MYSQL -h $NODE -BN -e 'show schemas like "tungsten%"' )
+    do
+        $MYSQL -h $NODE -e "drop schema $D"
+    done
+    $MYSQL -h $NODE -e 'drop schema if exists test'
+    $MYSQL -h $NODE -e 'drop schema if exists evaluator'
+    $MYSQL -h $NODE -e 'create schema test'
+    $MYSQL -h $NODE -e 'set global read_only=0'
+    $MYSQL -h $NODE -e 'set global binlog_format=mixed'
+    $MYSQL -h $NODE -e 'reset master'
+done
+
+[ -f $INSTALL_LOG ] && rm -f $INSTALL_LOG
+[ -f $CURRENT_TOPOLOGY ] && rm -f $CURRENT_TOPOLOGY
