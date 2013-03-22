1996861f2f9803b5a9e85861bfcd68431aaea409
g.maxia@gmail.com <g.maxia@gmail.com@d8f13b22-7da2-2874-8a57-a883d836a351>
2013-03-22 09:54:18 +0000

    - Fixed validate_cluster (No execution privilege) - added option to display validation with debug detail (VERBOSE=2 ./cookbook/validate_cluster.sh) - added check for installed topology for test_cluster and show_cluster

--- cookbook/validate_cluster.sh
+++ cookbook/validate_cluster.sh
@@ -2,9 +2,22 @@
 # (C) Copyright 2012 Continuent, Inc - Released under the New BSD License
 # Version 1.0.4 - 2013-03-07
 
-export VERBOSE=1
+[ -z "$VERBOSE" ]  && export VERBOSE=1
 export VALIDATE_ONLY=1
 COOKBOOK_DIR=$(dirname $0)
+
+if [ -f CURRENT_TOPOLOGY ]
+then
+    echo "There is already a topology installed : `cat CURRENT_TOPOLOGY`"
+    echo "The purpose of this script is to check the cluster before installation "
+    echo "to make sure that you can install"
+    echo ""
+    echo "Syntax: [VERBOSE=2] $0"
+    echo ""
+    echo "If VERBOSE=2 is used, you will get all the gory details of what Tungsten is checking"
+    exit 1
+fi
+
 
 if [ -f $COOKBOOK_DIR/install_master_slave.sh ]
 then
