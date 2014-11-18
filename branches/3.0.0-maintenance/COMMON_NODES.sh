66f814ef839f151db7b3212636ff21adbce3ae89
linas.virbalas@continuent.com <linas.virbalas@continuent.com@d8f13b22-7da2-2874-8a57-a883d836a351>
2014-11-18 15:31:55 +0000

    Creating maintenance branch for 3.0.0-524

---
+++ branches/3.0.0-maintenance/COMMON_NODES.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# (C) Copyright 2012,2013 Continuent, Inc - Released under the New BSD License
+# Version 1.0.5 - 2013-04-03
+
+export NODE1=host1
+export NODE2=host2
+export NODE3=host3
+export NODE4=host4
+#export NODE5=host5
+#export NODE6=host6
+#export NODE7=host7
+#export NODE8=host8
+
